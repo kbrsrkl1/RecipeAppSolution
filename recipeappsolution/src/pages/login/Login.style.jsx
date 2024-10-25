c88ef33f8dbfae1a8928de9e9b662b550056c0ef
kbrsrkl1 <kubranur216@gmail.com>
2024-10-26 00:24:04 +0300

    commit

---
+++ recipeappsolution/src/pages/login/Login.style.jsx
@@ -0,0 +1,13 @@
+import style from `styled-components`;
+import Flex from `../../companents/globalStyles/Flex.style`;
+
+
+export const LoginContainer= styled(Flex)`
+
+                height: 100vh;
+                background-color: ;
+                background-repeat:no-repeat;
+                background-size: cover;
+                background-position: center;
+
+`
