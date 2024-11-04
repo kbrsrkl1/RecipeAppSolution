2e04b4e624c009748255d07ad4a95c625faaf995
kbrsrkl1 <kubranur216@gmail.com>
2024-11-04 23:52:02 +0300

    commit

--- recipeappsolution/src/components/cards/Cards.style.jsx
+++ recipeappsolution/src/components/cards/Cards.style.jsx
@@ -1,49 +1,47 @@
 import styled from 'styled-components';
 import Flex from '../../components/globalStyles/Flex.style';
 
-
 export const MainContainer = styled(Flex)`
-        flex-direction: row;
-`
-
+  flex-direction: row;
+`;
 export const Card = styled(Flex)`
-        flex-direction: column;
-        height: 20rem;
-        width: 20rem;
-        background: ${({  theme }) => theme.colors.navbarBgColor};
-        padding: 1rem;
-        border-radius: 3px;
-        margin: 0.7rem;
-        box-shadow: 3px 3px 10px 1px rgba(0, 0, 0, 0.3);
-            &:hover {
-                box-shadow: none;
-                transition: all 0.3s ease-in;
-            }
+  flex-direction: column;
+  height: 22rem;
+  width: 22rem;
+  background: ${({ theme }) => theme.colors.navbarBgColor};
+  padding: 0.5rem;
+  border-radius: 3px;
+  margin: 0.7rem;
+  box-shadow: 3px 3px 10px 1px rgba(0, 0, 0, 0.3);
+  &:hover {
+    box-shadow: none;
+    transition: all 0.3s ease-in;
+  }
 `;
 
 export const Image = styled.img`
-        height: 10rem;
-        border-radius: 10px;
+  height: 10rem;
+  border-radius: 10px;
+  box-shadow: 1px 1px 7px 1px #4c4c4c;
 `;
 
 export const Button = styled.button`
-        background: ${({ theme }) => theme.colors.mainColor};
-        padding: 0.7rem;
-        outline: none;
-        border: none;
-        margin: 0.5rem;
-        border-radius: 3px;
-        cursor: pointer;
-            &:hover {
-                opacity: 0.6;
-            }
+  background: ${({ theme }) => theme.colors.mainColor};
+  padding: 0.8rem;
+  font-size: 1rem;
+  outline: none;
+  border: none;
+  margin: 0.9rem;
+  border-radius: 3px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.6;
+    background-color: #25c0c5;
+  }
 `;
 
 export const Header = styled.h1`
-        font-size: 1.5rem;
-        text-align: center;
+  font-size: 1.5rem;
+  text-align: center;
+  margin-bottom: 0.6rem;
 `;
-
-
-
-
